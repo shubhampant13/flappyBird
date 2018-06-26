bf6c4b99dfc8ceefad6acf0cd3578a0a8cbbe304
shubhampant13 <shubhampant13@gmail.com>
2018-06-26 11:18:29 +0530

    Working piece

--- flappyBird.js
+++ flappyBird.js
@@ -22,15 +22,69 @@ bg.addEventListener("load" , draw , false); // The image should load before the 
 
 
 // Drawing the images on the canvas
-var gap = 85;
-var constant = pipeNorth.height + gap;
+var gap = 350;
+var constant = pipeNorth.height+gap;
+var bX=10;
+var bY=150;
+var gravity = 1.2;
+var score = 0;
+var fly = new Audio();
+var scaudio =  new Audio();
+fly.src="sounds/fly.mp3";
+scaudio.src="sounds/score.mp3";
+
+// Event on pressing key
+document.addEventListener("keydown",moveUp);
+function moveUp() {
+    bY=bY-50;
+    fly.play();
+}
+// Event on pressing key ends
+
+// Creating pipes regularly to move from rtl
+var pipe = [];
+pipe[0]= { 
+      x : cvs.width,
+      y : 0
+};
+
 
 function draw() {
     ctx.drawImage(bg,0,0);
-    ctx.drawImage(pipeNorth,100,0)
-    ctx.drawImage(pipeSouth,100,0+constant);
-
-       
+    for(var i=0;i<pipe.length;i++)
+        {
+          ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
+          ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);
+          pipe[i].x--;    
+            
+          if(pipe[i].x == 125){
+              pipe.push({
+                  x : cvs.width ,
+                  y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height
+              });
+          }    
+            
+            if(bX+bird.width >= pipe[i].x && bX <= pipe[i].x+pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >= cvs.height-fg.height){
+                location.reload();
+            }
+            
+            if(pipe[i].x == 5)
+                {
+                 score++;
+                 scaudio.play();
+                }
+        
+            
+        }
+    
+    
+    ctx.drawImage(fg,0,cvs.height-fg.height);
+    ctx.drawImage(bird,bX,bY); 
+    bY = bY + gravity;
+    ctx.fillStyle="#000";
+    ctx.font="40px Verdana";
+    ctx.fillText("Score:" +score,10,cvs.height-30);
+    requestAnimationFrame(draw);
 }
 
 draw();
